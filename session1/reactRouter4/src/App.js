90f3896cef2b158c0d97ff493d4d19b938d9f79c
Fernando Rubio <fernando@wizeline.com>
2018-02-19 15:10:03 -0600

    scope /components

--- session1/reactRouter4/src/App.js
+++ session1/reactRouter4/src/App.js
@@ -30,10 +30,10 @@ class App extends Component {
             <li><NavLink to="/Introduction" activeClass="active">Introduction</NavLink></li>
             <li><NavLink to="/MentalModel" activeClass="active">Mental model change</NavLink></li>
             <li><NavLink to="/InitConfiguration" activeClass="active">Init configuration (#step1-init-config)</NavLink></li>
-            <li><NavLink to="/RouteComponent" activeClass="active">Route component (#step2-route-component)</NavLink></li>
-            <li><NavLink to="/LinkComponent" activeClass="active">Link component (#step3-link-component)</NavLink></li>
+            <li><NavLink to="/components/RouteComponent" activeClass="active">Route component (#step2-route-component)</NavLink></li>
+            <li><NavLink to="/components/LinkComponent" activeClass="active">Link component (#step3-link-component)</NavLink></li>
             <li><NavLink to="/DecentralizedPattern" activeClass="active">Decentralized pattern (#step4-decentralized-pattern)</NavLink></li>
-            <li><NavLink to="/RedirectComponent" activeClass="active">Redirect component (#step5-redirect-component)</NavLink></li>
+            <li><NavLink to="/components/RedirectComponent" activeClass="active">Redirect component (#step5-redirect-component)</NavLink></li>
             <li><NavLink to="/Authentication" activeClass="active">Authentication (#step6-authentication)</NavLink></li>
           </ul>
           <hr />
@@ -42,10 +42,10 @@ class App extends Component {
           <Route path="/Introduction" component={Introduction}  />
           <Route path="/MentalModel" component={MentalModel}  />
           <Route path="/InitConfiguration" component={InitConfiguration}  />
-          <Route path="/RouteComponent" component={RouteComponent}  />
-          <Route path="/LinkComponent" component={LinkComponent}  />
+          <Route path="/components/RouteComponent" component={RouteComponent}  />
+          <Route path="/components/LinkComponent" component={LinkComponent}  />
           <Route path="/DecentralizedPattern" component={DecentralizedPattern}  />
-          <Route path="/RedirectComponent" component={RedirectComponent}  />
+          <Route path="/components/RedirectComponent" component={RedirectComponent}  />
           <Route path="/Authentication" component={Authentication}  />
         </p>
       </div>
