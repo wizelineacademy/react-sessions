1ebfe67abcc5cbc737c43f182c68559e6516395f
Charlie <charlie@wizeline.com>
2018-02-28 16:57:06 -0600

    setup finished

--- session4/apollo/src/index.js
+++ session4/apollo/src/index.js
@@ -4,6 +4,26 @@ import "./index.css";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Apollo bindings!
+import { ApolloProvider as Provider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+// Here you create the HttpLink that will connect your ApolloClient instance with the GraphQL API; your GraphQL server will be running on http://localhost:4000.
+const httpLink = new HttpLink({ uri: "https://graphql-pokemon.now.sh/graphql" });
+
+// instantiate ApolloClient by passing in the httpLink and a new instance of an InMemoryCache
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <Provider client={client}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 registerServiceWorker();
