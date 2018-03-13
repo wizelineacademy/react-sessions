a4d81ef1ae2ac1a520a93f47eb9b5ea05e20ce4a
bit-dragon <jorge.em223@gmail.com>
2018-03-12 18:22:26 -0600

    Remove Tile error boundary implementation

--- session1/react16/src/js/components/presentational/TileErrorBoundary.js
+++ session1/react16/src/js/components/presentational/TileErrorBoundary.js
@@ -1,28 +1,7 @@
 import React, { Component } from 'react';
 import { ErrorTileStyled } from './Tile.styled';
 
-class TileErrorBoundary extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = { hasErrors: false, info: '' };
-  }
-
-  componentDidCatch(error, info) {
-    console.log('error', error, info);
-    this.setState({ hasErrors: true });
-  }
-
-  render() {
-    const { children } = this.props;
-    const { hasErrors } = this.state;
-
-    if (hasErrors) {
-      return <ErrorTileStyled />
-    }
-
-    return children;
-  }
-}
+// TODO: Implement the error boundary
+const TileErrorBoundary = null;
 
 export default TileErrorBoundary;
