ee36b3eca92c07e4da44b78c47e9e0a0d52e91a9
Salvador Elizarraras Montenegro <salvadorbfm@gmail.com>
2018-03-14 19:46:57 -0600

    Added explicit TODO

--- session3/jest/src/test/Image.test.js
+++ session3/jest/src/test/Image.test.js
@@ -1,13 +1,25 @@
+// Basic Snapshopt Testing
 import React from 'react';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 import Image from '../components/Image';
 
+// TODOs
+// Fix the current test
+// Test the prop alt
+
 describe('Image', () => {
   it('should render correctly', () => {
     const output = shallow(
-      <Image title="mockTitle" url="mockUrl" />
+      <Image title='mockTitle' url='mockUrl' />
     );
     expect(shallowToJson(output)).toMatchSnapshot();
+  });
+
+  it('should render with src', () => {
+    const output = shallow(
+      <Image title='mockTitle' url='mockUrl' />
+    );
+    expect(output.prop('src')).toEqual('mockUrl');
   });
 });
