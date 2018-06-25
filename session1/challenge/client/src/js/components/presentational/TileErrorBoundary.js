02cb01dd6013584c5405296d728f929e5023b6e5
bgilm <benjamingil94@gmail.com>
2018-06-25 18:41:50 -0500

    Implement error component

--- session1/challenge/client/src/js/components/presentational/TileErrorBoundary.js
+++ session1/challenge/client/src/js/components/presentational/TileErrorBoundary.js
@@ -1,6 +1,31 @@
 import React, { Component } from "react";
 import { DataTileStyled } from '../container/App.styled';
 
-const TileErrorBoundary = null;
-// TODO: implement error component with lifecycle method
+class TileErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasErrors: false,
+      info: '',
+    }
+  }
+
+    componentDidCatch(error, info) {
+      this.setState(() => ({
+        hasErrors: true,
+        info,
+      }));
+      console.warn('Error:', info);
+    }
+
+    render() {
+      const { children } = this.props;
+      const { hasErrors } = this.state;
+
+      if (hasErrors) {
+        return <DataTileStyled error/>
+      }
+      return children;
+    }
+  }
 export { TileErrorBoundary };
