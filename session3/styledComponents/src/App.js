c3ad6c016eedf5f960edec6c396c2243b269c9ce
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-07-09 21:25:52 -0500

    App.css removed from App.js only using styled components

--- session3/styledComponents/src/App.js
+++ session3/styledComponents/src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+import { injectGlobal } from 'styled-components';
 import NavbarList from './components/NavbarList';
+import { Nav } from './App.style';
+
+injectGlobal`
+ul {
+  0% { 
+   width:0;
+ }
+ 100% { 
+   width:calc(100% - 23px);
+ }
+}
+`;
 
 const getLinks = () => [
   { label: 'Home', url: '/' },
@@ -29,7 +41,7 @@ class App extends Component {
   }
 
   render() {
-    return <ul className="nav">{this.renderLinks()}</ul>;
+    return <Nav>{this.renderLinks()}</Nav>;
   }
 }
 
