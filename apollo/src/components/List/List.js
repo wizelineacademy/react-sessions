027a2bc223994af34bfe5994da1d12da2382a5f5
Charlie <charlie@wizeline.com>
2018-07-12 17:45:32 -0500

    comment code as a guide

--- apollo/src/components/List/List.js
+++ apollo/src/components/List/List.js
@@ -4,40 +4,26 @@ import Pokemon from "../Pokemon";
 import { POKEMONS_QUERY } from "./List.query";
 import { Container } from "./List.styled";
 
+//<Container>
+//{data.pokemons.map(pokemon => {
+
+//return (
+//<Pokemon
+//onSelect={onSelect}
+//key={pokemon.id}
+//id={pokemon.id}
+//name={pokemon.name}
+//image={pokemon.image}
+//number={pokemon.id}
+///>
+//);
+//})}
+//</Container>
+
 class PokemonList extends Component {
   render() {
     const { onSelect } = this.props;
-    return (
-      <Query query={POKEMONS_QUERY}>
-        {({ loading, error, data }) => {
-          if (error) {
-            return <div>error</div>;
-          }
-
-          if (loading) {
-            return <div> loading </div>;
-          }
-
-          return (
-            <Container>
-              {data.pokemons.map(pokemon => {
-
-                return (
-                  <Pokemon
-                    onSelect={onSelect}
-                    key={pokemon.id}
-                    id={pokemon.id}
-                    name={pokemon.name}
-                    image={pokemon.image}
-                    number={pokemon.id}
-                  />
-                );
-              })}
-            </Container>
-          );
-        }}
-      </Query>
-    );
+    return <div> Pokemon List </div>;
   }
 }
 
--- apollo/src/components/PokemonDetails/PokemonDetails.js
+++ apollo/src/components/PokemonDetails/PokemonDetails.js
@@ -8,42 +8,22 @@ import {
   DetailsList,
   DetailItem
 } from "./PokemonDetails.styled";
+//<Card>
+//<Image src={image} />
+//<Name>{name}</Name>
+//<DetailsList>
+//<DetailItem> Number: {number} </DetailItem>
+//<DetailItem> Classification: {classification} </DetailItem>
+//<DetailItem> Flee Rate: {fleeRate} </DetailItem>
+//<DetailItem> Max CP: {maxCP} </DetailItem>
+//</DetailsList>
+//</Card>
 
 class PokemonDetails extends Component {
   render() {
     const { id } = this.props;
 
-    return (
-      <Query query={POKEMON_QUERY} variables={{ id }}>
-        {({ loading, error, data }) => {
-          if (loading) {
-            return <div> Loading </div>;
-          }
-
-          const {
-            image,
-            name,
-            number,
-            classification,
-            fleeRate,
-            maxCP
-          } = data.pokemon;
-
-          return (
-            <Card>
-              <Image src={image} />
-              <Name>{name}</Name>
-              <DetailsList>
-                <DetailItem> Number: {number} </DetailItem>
-                <DetailItem> Classification: {classification} </DetailItem>
-                <DetailItem> Flee Rate: {fleeRate} </DetailItem>
-                <DetailItem> Max CP: {maxCP} </DetailItem>
-              </DetailsList>
-            </Card>
-          );
-        }}
-      </Query>
-    );
+    return <div> Pokemon Details</div>;
   }
 }
 
